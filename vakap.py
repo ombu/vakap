01d00e3cee624c938e64b8f1c75062f5a2bdfd38
Martin Rio <martin@ombuweb.com>
2011-11-02 21:57:02 -0700

    adds --filter option [closes #5]

--- vakap.py
+++ vakap.py
@@ -24,6 +24,8 @@ def main():
     parser.add_option("-s", "--settings", dest="settings",
             help="Hosts manifest JSON file [default: %default]",
             default="settings.json")
+    parser.add_option("-f", "--filter", dest="filter",
+            help="Comma-separated list of sites to process [default: all sites]")
 
     (options, args) = parser.parse_args()
     if len(args) == 0:
@@ -31,7 +33,8 @@ def main():
         print "Available commands: backup"
         sys.exit(0)
 
-    sites = parse_settings(options.settings)
+    sites = parse_settings(options)
+
     for command in args:
         try:
             getattr(GlobalCommand, command)(sites)
@@ -46,14 +49,22 @@ def main():
                 except AttributeError:
                     pass
 
-def parse_settings(jsonFile):
+def parse_settings(options):
+    f = open(options.settings)
     try:
-        f = open(jsonFile)
         config = json.load(f)
         env.key_filename = config['settings']['default_ssh_key']
         env.s3_bucket = config['settings']['backup_s3_bucket']
         env.gpg_key = config['settings']['gpg_key']
-        return config['hosts']
+
+        # if filter option was passed, use it to filter site list
+        if options.filter:
+            filter_sites = [x.strip() for x in options.filter.split(',')]
+            hosts = filter(lambda x: x['name'] in filter_sites, config['hosts'])
+        else:
+            hosts = config['hosts']
+
+        return hosts
     finally:
         f.close()
 
