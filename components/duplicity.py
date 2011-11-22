7f593757c9eb78caa8dc897bc85db61cb79c462e
Martin Rio <martin@ombuweb.com>
2011-11-22 10:54:12 -0800

    better duplicity command

--- components/duplicity.py
+++ components/duplicity.py
@@ -19,5 +19,7 @@ def backup_files(site_name, path):
     print "  - Running Duplicity on directory: %s" % path
     with hide('running', 'stdout'):
         run("AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s duplicity \
+                -v2 --volsize=256 --asynchronous-upload \
+                --exclude %s/logs \
                 --encrypt-key %s --full-if-older-than 30D %s %s" %
-            (env.s3_access_key, env.s3_secret, env.gpg_key, path, s3_dest))
+            (env.s3_access_key, env.s3_secret, path, env.gpg_key, path, s3_dest))
