f7007fd5bd0b0b943cf98e28631080d9b71afadb
Martin Rio <martin@ombuweb.com>
2013-02-10 19:06:58 -0300

    updates distribution

--- setup.py
+++ setup.py
@@ -7,16 +7,20 @@ from distutils.core import setup
 setup(
     name='Vakap',
     version='0.0.1',
-    author='OMBU',
+    author='Martin Rio - OMBU',
     author_email='martin@ombuweb.com',
+    description='''A tool for managing site backups''',
+    long_description=open('README.md').read(),
     packages=['vakap', 'vakap.components'],
-    scripts=['bin/vakap'],
+    entry_points={
+        'console_scripts': [
+            'vakap = vakap.vakap:main',
+        ]
+    },
     url='https://github.com/ombu/vakap',
     license='LICENSE.txt',
-    description='Utility to manage Web sites.',
-    long_description=open('README.md').read(),
     install_requires=[
-        "Fabric >=1.4.0",
-        "boto >=2.2.2",
+        "Fabric >=1.5.0",
+        "boto >=2.7.0",
     ],
 )
